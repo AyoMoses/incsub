b26bb4b0d8eae1f1cee2223e589fa176a3291fa9
ayoMoses <odukoyaayodeji@gmail.com>
2022-04-24 18:56:18 +0100

    auth page fix

--- src/pages/Onboarding/SignUp/SignUp.tsx
+++ src/pages/Onboarding/SignUp/SignUp.tsx
@@ -5,15 +5,28 @@ import styles from './SignUp.module.scss';
 function SignUp() {
   let navigate = useNavigate();
   return (
-    <div className={styles.wrapper}>
-      This is the landing for this site{' '}
-      <button
+    <div className={styles.auth}>
+      <div className={styles.authColOne}>
+        <div className={styles.authSteps}>
+          <p className={styles.authStepsText}>Step 1 of 3</p>
+          <div className={styles.authStepsDots}>
+            <span className={styles.authStepsDotActive} />
+            <span className={styles.authStepsDot} />
+            <span className={styles.authStepsDot} />
+          </div>
+        </div>
+      </div>
+      <div className={styles.authColTwo}>
+        <h1 className={styles.authColTwoHeader}>dummy header</h1>
+        <p className={styles.authColTwoSubHeader}></p>
+      </div>
+      {/* <button
         onClick={() => {
           navigate('/sign-in');
         }}
       >
         sign in
-      </button>
+      </button> */}
     </div>
   );
 }
